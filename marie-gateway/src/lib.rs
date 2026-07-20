5ba8d10f8e4c3a6986deacd10f8356562dfbc6dd
gpabois <gael.pabois@gmail.com>
2026-07-20 22:55:15 +0200

    up

--- marie-gateway/src/lib.rs
+++ marie-gateway/src/lib.rs
@@ -1,16 +1,28 @@
-use marie_core::{layer::Layer, network::actor::{NetworkCommand, NetworkEvent}, session::{SessionEvent, SessionId}, workspace::{WorkspaceEvent, WorkspaceId}};
+use std::sync::Arc;
 
-pub enum GatewayCommand {
+use futures::future::BoxFuture;
+use marie_core::{layer::Layer, network::actor::{NetworkCommand, NetworkEvent}, session::{SessionEvent, SessionId}, workspace::{WorkspaceEvent, WorkspaceId}};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ForeignCommand {
     AccessSession(SessionId),
     AccessWorkspace(WorkspaceId)
 }
-pub enum GatewayEvent {
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ForeignEvent {
     Session(SessionEvent),
     Workspace(WorkspaceEvent)
 }
 
-pub struct GatewayArgs {
+pub enum LocalCommand {
     
+}
+
+pub struct GatewayArgs {
+    check_session: Arc<dyn Fn(SessionId) -> BoxFuture<'static, anyhow::Result<bool>>>,
+    check_workspace: Arc<dyn Fn(WorkspaceId) -> BoxFuture<'static, anyhow::Result<bool>>>
 }
 
 pub struct GatewayActor;
@@ -20,6 +32,19 @@ impl GatewayActor {
         layer: impl Layer<Send = NetworkCommand, Received = NetworkEvent>,
         foreign: impl Layer<Send = GatewayEvent, Received = GatewayCommand>
     ) {
-        
+        let (tx, rx) = layer.boxed_split();
+
+        tokio::spawn(async move {
+            loop {
+                tokio::select! {
+                    Some(event) = rx.event => {
+                        
+
+                    }
+                }
+            }
+        });
+
+
     }
 }
